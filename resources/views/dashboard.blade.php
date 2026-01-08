c8caea1390cba43024716a5e13d3f2ad65efdaff
camilo.cabrera <camilo.cabrera@vicepresidencia.gob.bo>
2026-01-08 16:36:37 -0400

    fix: add option to go to items once logged in

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h2 class="text-lg font-semibold mb-4">
+                        {{ __("You're logged in!") }}
+                    </h2>
+
+                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                        <a href="{{  url('/items') }}"
+                           class="block p-6 bg-white rounded-lg shadow hover:shadow-md transition">
+                            <h3 class="text-lg font-medium text-gray-900">
+                                Items
+                            </h3>
+                            <p class="text-sm text-gray-500">
+                                Manage your items (create, edit, delete)
+                            </p>
+                        </a>
+                    </div>
                 </div>
+
             </div>
         </div>
     </div>
